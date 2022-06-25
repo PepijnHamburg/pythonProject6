e5a64e0b5fa67881dd1a11821607ab8ec3dd1e4e
PepijnHamburg <104499352+PepijnHamburg@users.noreply.github.com>
2022-06-25 17:05:04 +0200

    tryin to push

--- instagram/t.R
+++
@@ -1,9 +0,0 @@
-from math import ceil, floor
-
-perimeter = 20
-length = int(ceil(perimeter / 4))
-breadth = int(floor(perimeter / 4))
-area = length * breadth
-print("length = " + str(length))
-print("breadth = " + str(breadth))
-print("area = " + str(area))
